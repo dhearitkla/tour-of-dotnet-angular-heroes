04c9c9fd8deaa5983e76ae3b2a14060c93083960
DannyHearit <d.hearit@kunzleigh.com>
2024-09-20 10:42:57 -0400

    removed extra variable

--- Entities/Models/Model.cs
+++ Entities/Models/Model.cs
@@ -7,12 +7,10 @@ public class HeroContext : DbContext
     public DbSet<Team> Teams { get; set; }
     public DbSet<Hero> Heroes { get; set; }
     public DbSet<Superpower> SuperPowers { get; set; }
-    public IConfiguration Configuration { get; }
     
     private string ConnectionString { get; }
     public HeroContext(IConfiguration configuration)
     {
-        Configuration = configuration;
         ConnectionString = configuration.GetSection("ConnectionStrings").GetSection("TourOfHeroes").Value;
     }
     protected override void OnConfiguring(DbContextOptionsBuilder options)
